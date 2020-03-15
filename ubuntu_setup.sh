b3e6239adc4dfddae695c4c537396a06f5afafec
hajeklu <40526897+hajeklu@users.noreply.github.com>
2020-03-15 20:02:25 +0100

    Fix -y and chromium

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -5,36 +5,35 @@ sudo apt upgrade -y
 sudo snap install chromium 
 
 #spotify
-sudo apt install snapd
 sudo snap install spotify
 
 #remmina
 sudo yes "" | apt-add-repository ppa:remmina-ppa-team/remmina-next
 sudo apt-get update
-sudo apt-get install remmina remmina-plugin-rdp libfreerdp-plugins-standard
+sudo apt-get -y install remmina remmina-plugin-rdp libfreerdp-plugins-standard
 
 #mailspring
 sudo snap install mailspring
 
 #gnome-tweak-tool
-sudo apt-get install gnome-tweak-tool
+sudo apt-get -y install gnome-tweak-tool
 sudo yes "" | add-apt-repository ppa:webupd8team/gnome3
 sudo apt-get update
-sudo apt-get install gnome-shell-extensions-user-themes
+sudo apt-get -y install gnome-shell-extensions-user-themes
 
 #vlc
-sudo apt-get install vlc
+sudo apt-get -y install vlc
 
 #gnome-extensions
-sudo apt install gnome-shell-extensions
-sudo apt install chrome-gnome-shell
+sudo apt -y install gnome-shell-extensions
+sudo apt -y install chrome-gnome-shell
 
 #nitrogen
 sudo apt-get install -y nitrogen
 nitrogen ~/ubuntu_setup
 
 #nemo x nautilus
-sudo apt install nemo
+sudo apt -y install nemo
 xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
 gsettings set org.gnome.desktop.background show-desktop-icons false
 gsettings set org.nemo.desktop show-desktop-icons true
@@ -47,26 +46,26 @@ sudo apt install -y ttf-mscorefonts-installer
 #exfat support 18.04
 sudo yes "" | add-apt-repository universe
 sudo apt update
-sudo apt install exfat-fuse exfat-utils
+sudo apt -y install exfat-fuse exfat-utils
 
 #gimp
-sudo add-apt-repository ppa:otto-kesselgulasch/gimp
+sudo yes "" | add-apt-repository ppa:otto-kesselgulasch/gimp
 sudo apt-get update
-sudo apt-get install gimp
+sudo apt-get -y install gimp
 
 #onedrive
-sudo apt install onedrive
+sudo apt -y install onedrive
 sudo mkdir ~/OneDrive
 #onedrive -m 
 
 #icons
 sudo mkdir ~/.icons
-chrome https://www.gnome-look.org/p/1305429
+chromium https://www.gnome-look.org/p/1305429
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 
 #dash to panel
-chrome https://extensions.gnome.org/extension/1160/dash-to-panel/
+chromium https://extensions.gnome.org/extension/1160/dash-to-panel/
 
 #reset gnome
 sudo killall -3 gnome-shell
